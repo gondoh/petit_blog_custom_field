7d4b0e46700034b5dbd766ee9b0328b9c7ff2541
arata <contact@materializing.net>
2013-06-03 02:13:42 +0900

    first プラグイン用ファイルを準備

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * [ADMIN] petit_blog_custom_field
+ *
+ * @copyright		Copyright 2013, materializing.
+ * @link			http://www.materializing.net/
+ * @author			arata
+ * @license			MIT
+ */
+$title = 'プチ・ブログ用カスタムフィールドプラグイン';
+$description = 'ブログ記事に入力欄を設定できます。';
+$author = 'arata';
+$url = 'http://www.materializing.net/';
+$adminLink = array('plugin' => 'petit_blog_custom_field', 'controller' => 'petit_blog_custom_field_configs', 'action' => 'index');
+$installMessage = 'ブログ記事の投稿画面に入力欄が追加されます。';
