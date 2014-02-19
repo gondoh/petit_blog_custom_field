2a896dee4067910ee0bf04bc1e8d62578bfcca55
arata <contact@materializing.net>
2014-02-20 05:46:03 +0900

    add 設定項目をブログ別に指定できるように変更

--- Config/petit_blog_custom_field.php
+++ Config/petit_blog_custom_field.php
@@ -41,12 +41,42 @@ $config['BcApp.adminNavi.petit_blog_custom_field'] = array(
  * 
  */
 $config['petitBlogCustomField.field_name'] = array(
-	'name'		=> 'テキスト',
-	'name_2'	=> 'テキスト2',
-	'textarea'	=> 'テキストエリア',
-	'radio'		=> 'ラジオ',
-	'select'	=> 'セレクト',
-	'date'		=> '日付'
+	1 => array(
+		'name'		=> 'テキスト',
+		'name_2'	=> 'テキスト2',
+		'textarea'	=> 'テキストエリア',
+		'radio'		=> 'ラジオ',
+		'select'	=> 'セレクト',
+		'date'		=> '日付',
+		'text_sub_1' => 'サブテキスト01',
+		'text_sub_2' => 'サブテキスト02',
+		'text_sub_3' => 'サブテキスト03',
+		'text_sub_4' => 'サブテキスト04',
+		'text_sub_5' => 'サブテキスト05',
+		'text_sub_6' => 'サブテキスト06',
+		'text_sub_7' => 'サブテキスト07',
+		'text_sub_8' => 'サブテキスト08',
+		'text_sub_9' => 'サブテキスト09',
+		'text_sub_10' => 'サブテキスト10'
+	),
+	2 => array(
+		'name'		=> 'テキスト',
+		'name_2'	=> 'テキスト2',
+		'textarea'	=> 'テキストエリア',
+		'radio'		=> 'ラジオ',
+		'select'	=> 'セレクト',
+		'date'		=> '日付',
+		'text_sub_1' => 'サブテキスト01',
+		'text_sub_2' => 'サブテキスト02',
+		'text_sub_3' => 'サブテキスト03',
+		'text_sub_4' => 'サブテキスト04',
+		'text_sub_5' => 'サブテキスト05',
+		'text_sub_6' => 'サブテキスト06',
+		'text_sub_7' => 'サブテキスト07',
+		'text_sub_8' => 'サブテキスト08',
+		'text_sub_9' => 'サブテキスト09',
+		'text_sub_10' => 'サブテキスト10'
+	)
 );
 /**
  * プチ・カスタムフィールドデフォルト設定
@@ -57,25 +87,31 @@ $config['petitBlogCustomField.field_name'] = array(
  * 
  */
 $config['petitBlogCustomField.status'] = array(
-	0 => '利用しない',
-	1 => '利用する'
+	1 => array(
+		0 => '利用しない',
+		1 => '利用する'
+	)
 );
 /**
  * ラジオ設定
  * 
  */
 $config['petitBlogCustomField.radio'] = array(
-	0 => '指定無し',
-	1 => 'ラジオ1',
-	2 => 'ラジオ2'
+	1 => array(
+		0 => '指定無し',
+		1 => 'ラジオ1',
+		2 => 'ラジオ2'
+	)
 );
 /**
  * セレクト設定
  * 
  */
 $config['petitBlogCustomField.select'] = array(
-	0 => '指定無し',
-	1 => 'セレクト1',
-	2 => 'セレクト2',
-	3 => 'セレクト3'
+	1 => array(
+		0 => '指定無し',
+		1 => 'セレクト1',
+		2 => 'セレクト2',
+		3 => 'セレクト3'
+	)
 );
--- View/Elements/admin/petit_blog_custom_field_form.php
+++ View/Elements/admin/petit_blog_custom_field_form.php
@@ -7,6 +7,11 @@
  * @package			PetitBlogCustomField
  * @license			MIT
  */
+if ($this->request->params['controller'] == 'blog_posts') {
+	$blogContentId = $blogContent['BlogContent']['id'];
+} else {
+	$blogContentId = $this->request->data['PetitBlogCustomField']['blog_content_id'];
+}
 $style = '';
 ?>
 <?php if($this->request->params['controller'] == 'blog_posts'): ?>
@@ -72,7 +77,7 @@ $(window).load(function() {
 		<td class="col-input">
 			<?php echo $this->BcForm->input('PetitBlogCustomField.status', array(
 					'type'		=> 'radio',
-					'options'	=> $customFieldConfig['status'],
+					'options'	=> $customFieldConfig['status'][$blogContentId],
 					'legend'	=> false,
 					'separator'	=> '&nbsp;&nbsp;')) ?>
 			<?php echo $this->BcForm->error('PetitBlogCustomField.status') ?>
@@ -81,7 +86,7 @@ $(window).load(function() {
 <?php if($this->request->data['PetitBlogCustomFieldConfig']['use_name']): ?>
 	<tr>
 		<th class="col-head">
-			<?php echo $this->BcForm->label('PetitBlogCustomField.name', $customFieldConfig['field_name']['name']) ?>
+			<?php echo $this->BcForm->label('PetitBlogCustomField.name', $customFieldConfig['field_name'][$blogContentId]['name']) ?>
 		</th>
 		<td class="col-input">
 			<?php echo $this->BcForm->input('PetitBlogCustomField.name', array('type' => 'text', 'size' => 40, 'maxlength' => 255, 'counter' => true)) ?>
@@ -92,7 +97,7 @@ $(window).load(function() {
 <?php if($this->request->data['PetitBlogCustomFieldConfig']['use_name_2']): ?>
 	<tr>
 		<th class="col-head">
-			<?php echo $this->BcForm->label('PetitBlogCustomField.name_2', $customFieldConfig['field_name']['name_2']) ?>
+			<?php echo $this->BcForm->label('PetitBlogCustomField.name_2', $customFieldConfig['field_name'][$blogContentId]['name_2']) ?>
 		</th>
 		<td class="col-input">
 			<?php echo $this->BcForm->input('PetitBlogCustomField.name_2', array('type' => 'text', 'size' => 40, 'maxlength' => 255, 'counter' => true)) ?>
@@ -103,7 +108,7 @@ $(window).load(function() {
 <?php if($this->request->data['PetitBlogCustomFieldConfig']['use_content']): ?>
 	<tr>
 		<th class="col-head">
-			<?php echo $this->BcForm->label('PetitBlogCustomField.content', $customFieldConfig['field_name']['textarea']) ?>
+			<?php echo $this->BcForm->label('PetitBlogCustomField.content', $customFieldConfig['field_name'][$blogContentId]['textarea']) ?>
 		</th>
 		<td class="col-input">
 			<?php echo $this->BcForm->ckeditor('PetitBlogCustomField.content', array(
@@ -120,10 +125,10 @@ $(window).load(function() {
 <?php if($this->request->data['PetitBlogCustomFieldConfig']['use_radio']): ?>
 	<tr>
 		<th class="col-head">
-			<?php echo $this->BcForm->label('PetitBlogCustomField.radio', $customFieldConfig['field_name']['radio']) ?>
+			<?php echo $this->BcForm->label('PetitBlogCustomField.radio', $customFieldConfig['field_name'][$blogContentId]['radio']) ?>
 		</th>
 		<td class="col-input">
-			<?php echo $this->BcForm->input('PetitBlogCustomField.radio', array('type' => 'radio', 'options' => $customFieldConfig['radio'])) ?>
+			<?php echo $this->BcForm->input('PetitBlogCustomField.radio', array('type' => 'radio', 'options' => $customFieldConfig['radio'][$blogContentId])) ?>
 			<?php echo $this->BcForm->error('PetitBlogCustomField.radio') ?>
 		</td>
 	</tr>
@@ -131,10 +136,10 @@ $(window).load(function() {
 <?php if($this->request->data['PetitBlogCustomFieldConfig']['use_select']): ?>
 	<tr>
 		<th class="col-head">
-			<?php echo $this->BcForm->label('PetitBlogCustomField.select', $customFieldConfig['field_name']['select']) ?>
+			<?php echo $this->BcForm->label('PetitBlogCustomField.select', $customFieldConfig['field_name'][$blogContentId]['select']) ?>
 		</th>
 		<td class="col-input">
-			<?php echo $this->BcForm->input('PetitBlogCustomField.select', array('type' => 'select', 'options' => $customFieldConfig['select'])) ?>
+			<?php echo $this->BcForm->input('PetitBlogCustomField.select', array('type' => 'select', 'options' => $customFieldConfig['select'][$blogContentId])) ?>
 			<?php echo $this->BcForm->error('PetitBlogCustomField.select') ?>
 		</td>
 	</tr>
@@ -142,7 +147,7 @@ $(window).load(function() {
 <?php if($this->request->data['PetitBlogCustomFieldConfig']['use_date']): ?>
 	<tr>
 		<th class="col-head">
-			<?php echo $this->BcForm->label('PetitBlogCustomField.date', $customFieldConfig['field_name']['date']) ?>
+			<?php echo $this->BcForm->label('PetitBlogCustomField.date', $customFieldConfig['field_name'][$blogContentId]['date']) ?>
 		</th>
 		<td class="col-input">
 			<?php echo $this->BcForm->dateTimePicker('PetitBlogCustomField.date', array('size' => 12, 'maxlength' => 10), true) ?>
@@ -150,5 +155,117 @@ $(window).load(function() {
 		</td>
 	</tr>
 <?php endif ?>
+
+<?php if($this->request->data['PetitBlogCustomFieldConfig']['use_text_sub_1']): ?>
+	<tr>
+		<th class="col-head">
+			<?php echo $this->BcForm->label('PetitBlogCustomField.text_sub_1', $customFieldConfig['field_name'][$blogContentId]['text_sub_1']) ?>
+		</th>
+		<td class="col-input">
+			<?php echo $this->BcForm->input('PetitBlogCustomField.text_sub_1', array('type' => 'text', 'size' => 60, 'maxlength' => 255, 'counter' => true)) ?>
+			<?php echo $this->BcForm->error('PetitBlogCustomField.text_sub_1') ?>
+		</td>
+	</tr>
+<?php endif ?>
+<?php if($this->request->data['PetitBlogCustomFieldConfig']['use_text_sub_2']): ?>
+	<tr>
+		<th class="col-head">
+			<?php echo $this->BcForm->label('PetitBlogCustomField.text_sub_2', $customFieldConfig['field_name'][$blogContentId]['text_sub_2']) ?>
+		</th>
+		<td class="col-input">
+			<?php echo $this->BcForm->input('PetitBlogCustomField.text_sub_2', array('type' => 'text', 'size' => 60, 'maxlength' => 255, 'counter' => true)) ?>
+			<?php echo $this->BcForm->error('PetitBlogCustomField.text_sub_2') ?>
+		</td>
+	</tr>
+<?php endif ?>
+<?php if($this->request->data['PetitBlogCustomFieldConfig']['use_text_sub_3']): ?>
+	<tr>
+		<th class="col-head">
+			<?php echo $this->BcForm->label('PetitBlogCustomField.text_sub_3', $customFieldConfig['field_name'][$blogContentId]['text_sub_3']) ?>
+		</th>
+		<td class="col-input">
+			<?php echo $this->BcForm->input('PetitBlogCustomField.text_sub_3', array('type' => 'text', 'size' => 60, 'maxlength' => 255, 'counter' => true)) ?>
+			<?php echo $this->BcForm->error('PetitBlogCustomField.text_sub_3') ?>
+		</td>
+	</tr>
+<?php endif ?>
+<?php if($this->request->data['PetitBlogCustomFieldConfig']['use_text_sub_4']): ?>
+	<tr>
+		<th class="col-head">
+			<?php echo $this->BcForm->label('PetitBlogCustomField.text_sub_4', $customFieldConfig['field_name'][$blogContentId]['text_sub_4']) ?>
+		</th>
+		<td class="col-input">
+			<?php echo $this->BcForm->input('PetitBlogCustomField.text_sub_4', array('type' => 'text', 'size' => 60, 'maxlength' => 255, 'counter' => true)) ?>
+			<?php echo $this->BcForm->error('PetitBlogCustomField.text_sub_4') ?>
+		</td>
+	</tr>
+<?php endif ?>
+<?php if($this->request->data['PetitBlogCustomFieldConfig']['use_text_sub_5']): ?>
+	<tr>
+		<th class="col-head">
+			<?php echo $this->BcForm->label('PetitBlogCustomField.text_sub_5', $customFieldConfig['field_name'][$blogContentId]['text_sub_5']) ?>
+		</th>
+		<td class="col-input">
+			<?php echo $this->BcForm->input('PetitBlogCustomField.text_sub_5', array('type' => 'text', 'size' => 60, 'maxlength' => 255, 'counter' => true)) ?>
+			<?php echo $this->BcForm->error('PetitBlogCustomField.text_sub_5') ?>
+		</td>
+	</tr>
+<?php endif ?>
+<?php if($this->request->data['PetitBlogCustomFieldConfig']['use_text_sub_6']): ?>
+	<tr>
+		<th class="col-head">
+			<?php echo $this->BcForm->label('PetitBlogCustomField.text_sub_6', $customFieldConfig['field_name'][$blogContentId]['text_sub_6']) ?>
+		</th>
+		<td class="col-input">
+			<?php echo $this->BcForm->input('PetitBlogCustomField.text_sub_6', array('type' => 'text', 'size' => 60, 'maxlength' => 255, 'counter' => true)) ?>
+			<?php echo $this->BcForm->error('PetitBlogCustomField.text_sub_6') ?>
+		</td>
+	</tr>
+<?php endif ?>
+<?php if($this->request->data['PetitBlogCustomFieldConfig']['use_text_sub_7']): ?>
+	<tr>
+		<th class="col-head">
+			<?php echo $this->BcForm->label('PetitBlogCustomField.text_sub_7', $customFieldConfig['field_name'][$blogContentId]['text_sub_7']) ?>
+		</th>
+		<td class="col-input">
+			<?php echo $this->BcForm->input('PetitBlogCustomField.text_sub_7', array('type' => 'text', 'size' => 60, 'maxlength' => 255, 'counter' => true)) ?>
+			<?php echo $this->BcForm->error('PetitBlogCustomField.text_sub_7') ?>
+		</td>
+	</tr>
+<?php endif ?>
+<?php if($this->request->data['PetitBlogCustomFieldConfig']['use_text_sub_8']): ?>
+	<tr>
+		<th class="col-head">
+			<?php echo $this->BcForm->label('PetitBlogCustomField.text_sub_8', $customFieldConfig['field_name'][$blogContentId]['text_sub_8']) ?>
+		</th>
+		<td class="col-input">
+			<?php echo $this->BcForm->input('PetitBlogCustomField.text_sub_8', array('type' => 'text', 'size' => 60, 'maxlength' => 255, 'counter' => true)) ?>
+			<?php echo $this->BcForm->error('PetitBlogCustomField.text_sub_8') ?>
+		</td>
+	</tr>
+<?php endif ?>
+<?php if($this->request->data['PetitBlogCustomFieldConfig']['use_text_sub_9']): ?>
+	<tr>
+		<th class="col-head">
+			<?php echo $this->BcForm->label('PetitBlogCustomField.text_sub_9', $customFieldConfig['field_name'][$blogContentId]['text_sub_9']) ?>
+		</th>
+		<td class="col-input">
+			<?php echo $this->BcForm->input('PetitBlogCustomField.text_sub_9', array('type' => 'text', 'size' => 60, 'maxlength' => 255, 'counter' => true)) ?>
+			<?php echo $this->BcForm->error('PetitBlogCustomField.text_sub_9') ?>
+		</td>
+	</tr>
+<?php endif ?>
+<?php if($this->request->data['PetitBlogCustomFieldConfig']['use_text_sub_10']): ?>
+	<tr>
+		<th class="col-head">
+			<?php echo $this->BcForm->label('PetitBlogCustomField.text_sub_10', $customFieldConfig['field_name'][$blogContentId]['text_sub_10']) ?>
+		</th>
+		<td class="col-input">
+			<?php echo $this->BcForm->input('PetitBlogCustomField.text_sub_10', array('type' => 'text', 'size' => 60, 'maxlength' => 255, 'counter' => true)) ?>
+			<?php echo $this->BcForm->error('PetitBlogCustomField.text_sub_10') ?>
+		</td>
+	</tr>
+<?php endif ?>
+
 </table>
 </div>
