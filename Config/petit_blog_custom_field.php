8999b1563fa1fe6be5cd5452da0e3d352f252906
arata <contact@materializing.net>
2014-05-24 18:51:25 +0900

    change 設定ファイルを整理 - 設定一覧はシステム管理ユーザー利用時に表示されるように変更

--- Config/petit_blog_custom_field.php
+++ Config/petit_blog_custom_field.php
@@ -8,28 +8,6 @@
  * @license			MIT
  */
 /**
- * システムナビ
- */
-$config['BcApp.adminNavi.petit_blog_custom_field'] = array(
-		'name'		=> 'プチ・ブログカスタムフィールドプラグイン',
-		'contents'	=> array(
-			array('name' => 'ブログカスタムフィールド一覧',
-				'url' => array(
-					'admin' => true,
-					'plugin' => 'petit_blog_custom_field',
-					'controller' => 'petit_blog_custom_fields',
-					'action' => 'index')
-			),
-			array('name' => 'ブログカスタムフィールド設定一覧',
-				'url' => array(
-					'admin' => true,
-					'plugin' => 'petit_blog_custom_field',
-					'controller' => 'petit_blog_custom_field_configs',
-					'action' => 'index')
-			)
-	)
-);
-/**
  * フィールド名設定
  * 
  */
